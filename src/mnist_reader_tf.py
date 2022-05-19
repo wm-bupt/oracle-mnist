4bf43c6ef16038dfe18049d72e2441e2943d22d9
wm-bupt <62639571+wm-bupt@users.noreply.github.com>
2022-05-19 16:07:09 +0800

    Update mnist_reader_tf.py

--- src/mnist_reader_tf.py
+++ src/mnist_reader_tf.py
@@ -1,4 +1,4 @@
-"""Functions for downloading and reading MNIST data."""
+"""Functions for downloading and reading Oracle-MNIST data."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
